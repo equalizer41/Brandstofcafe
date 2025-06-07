b33913f6452337dd7e21770596a6d2f6551a2cec
equalizer41 <jorisvanveen@hotmail.com>
2025-06-07 12:30:41 +0200

    Nieuwe pagina's

--- projectweb/Models/AddOnCategorie.cs
+++ projectweb/Models/AddOnCategorie.cs
@@ -3,4 +3,7 @@ public class AddOnCategorie
 {
     public int Id { get; set; }
     public string Naam { get; set; }
+    public bool MeerdereToegestaan { get; set; }
+    public List<AddOn> Opties { get; set; }
+
 }
